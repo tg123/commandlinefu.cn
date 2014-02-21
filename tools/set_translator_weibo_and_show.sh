0785e9c2e2af2ac2f5d90c9039d414ed94e4b478
tgic <farmer1992@gmail.com>
2014-02-22 00:41:34 +0800

    add to replace param

--- tools/set_translator_weibo_and_show.sh
+++ tools/set_translator_weibo_and_show.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TRANSLATOR_WEIBO=$1
+TO_REPLACE=${2:-"''"}
+
 
 if [ -z $TRANSLATOR_WEIBO ];then
 
@@ -17,7 +19,7 @@ LIST=`git status -s $DATADIR | grep "^ ?M" -P | cut -d ' ' -f 3`
 for f in $LIST;do
     if [ -f $f ]; then
         echo "SET $f Translator WEIBO to $TRANSLATOR_WEIBO"
-        sed -i "s/weibo: ''/weibo: $TRANSLATOR_WEIBO/g" $f
+        sed -i "s/weibo: $TO_REPLACE\$/weibo: $TRANSLATOR_WEIBO/g" $f
         sed -i "/^hide: true$/d" $f
     fi
 done
