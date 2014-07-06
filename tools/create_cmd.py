d7bfc1abe8b65cb76bbd049470dc32b4191d9fb7
particle128 <mashu.dlut@gmail.com>
2014-07-06 20:23:46 +0800

    a python script to create your own command

---
+++ tools/create_cmd.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+import sys
+import string
+import os
+
+content='''commandlinefu_id: 0000
+translator:
+  weibo: ''
+hide: true
+command: |-
+  %(cmd)s
+summary: |-
+  %(trans)s'''
+
+def create(cmd,trans):
+    "create new yaml file"
+    filename=''
+    for token in cmd.split():
+        # reserve ascii letters and digits only
+        filename+=token.translate(None,token.translate(None,string.ascii_letters+string.digits))+'_'
+    filename=filename[0:len(filename)-1]+'.yaml'
+    pathname=sys.path[0][0:sys.path[0].rfind('/')]+'/_data/'+filename
+
+    try:
+        wfile=open(pathname,'w')
+        try:
+            wfile.write(content % {"cmd":cmd,"trans":trans})
+        except Exception as e:
+            wfile.close()
+            os.system('rm '+pathname)
+            print 'write file error:'
+            print e
+        else:
+            print 'create success...'
+            # can't place wfile.close() to finally block, since 'git add' should execute after close()
+            wfile.close() 
+            os.system('git add '+pathname)
+    except Exception as e:
+        print 'open file in write mode error:'
+        print e
+    
+    
+
+if __name__ == '__main__':
+
+    cmd=''
+    trans=''
+    while not cmd:
+        cmd=raw_input("Input the new command:")
+    while not trans:
+        trans=raw_input("input the translation:")
+    create(cmd,trans)
+    
