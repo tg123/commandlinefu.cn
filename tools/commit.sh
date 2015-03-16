7adf9dd122ff26a07d0f0b4b7d016df7a117528a
spacewander <spacewanderlzx@gmail.com>
2015-03-16 17:34:22 +0800

    Add user confirmation and commit all change under _data/

--- tools/commit.sh
+++ tools/commit.sh
@@ -10,10 +10,26 @@ if [ -z "$(git status)" ]; then
 fi
 
 # current directory will be the root directory
-# import the global variable such as $CONFIG
+# import the global variable such as $GIT_CONFIG
 . tools/config.sh
 
 ./tools/set_translator_weibo_and_show.sh
-git add _data/
+echo 'Press RETURN to commit your translation or use CTRL-C to leave'
+read # stop here and read the RETURN
+
+# if COMMIT_DATA_CHANGE is ALL: git add all change
+# else: git add Modified files
+DEFAULT_COMMIT_SETTING="COMMIT_DATA_CHANGE=ALL"
+if [ -f "$GIT_CONFIG" ]; then
+    . "$GIT_CONFIG"
+else
+    echo "$DEFAULT_COMMIT_SETTING" > "$GIT_CONFIG"
+fi
+
+if [ "${COMMIT_DATA_CHANGE=ALL}" == "ALL" ]; then
+    git add -A _data/
+else
+    git add _data/
+fi
 git commit 
 
